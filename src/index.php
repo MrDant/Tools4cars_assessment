c2791956859846f4af680c33d834d5403ba1e8e5
Mael DANT <mael.dantuono@gmail.com>
2025-08-10 10:47:13 +0200

    feat(content-display): display file content using jQuery

--- src/index.php
+++ src/index.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tool4cars</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="scripts/step1.js"></script>
 </head>
 <body>
     <div class="dynamic-div" data-module="cars" data-script="ajax"></div>
+    <!-- Ajouter des liens pour changer de client -->
+    <a href="#" class="change-client" data-client="clienta">Client A</a>
+    <a href="#" class="change-client" data-client="clientb">Client B</a>
+    <a href="#" class="change-client" data-client="clientc">Client C</a>
 </body>
 </html>
