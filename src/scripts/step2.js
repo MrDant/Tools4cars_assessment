ff74b6652e09bf45b1a29622b35d61c9fd196ccb
Mael DANT <mael.dantuono@gmail.com>
2025-08-10 12:23:17 +0200

    bugfix(client-car-list-view) : display all cars

--- src/scripts/step2.js
+++ src/scripts/step2.js
@@ -13,15 +13,17 @@ async function loadCarData(client) {
 
   // Charger et afficher les données des voitures
   $.getJSON("/data/cars.json", function (data) {
-    const car = data.filter((car) => car.customer === client).pop();
     const carsContent = $("#cars-content");
     carsContent.empty();
 
-    let carInfo = `<div class="car-item" data-id=${car.id}>`;
-
-    switch (client) {
-      case "clienta":
-        carInfo += `
+    let carInfo = "";
+    const car = data
+      .filter((car) => car.customer === client)
+      .forEach((car) => {
+        carInfo += `<div class="car-item" data-id=${car.id}>`;
+        switch (client) {
+          case "clienta":
+            carInfo += `
                     <p>Nom: ${car.modelName}</p>
                     <p>Marque: ${car.brand}</p>
                     <p>Année: ${new Date(car.year, 0, 1).toLocaleDateString(
@@ -30,28 +32,30 @@ async function loadCarData(client) {
                     )}</p>
                     <p>Puissance: ${car.power} chevaux</p>
             `;
-        break;
-      case "clientb":
-        const garage = garages.find((e) => e.id == car.garageId);
-        carInfo += `
+            break;
+          case "clientb":
+            const garage = garages.find((e) => e.id == car.garageId);
+            carInfo += `
                     <p>Nom: ${car.modelName.toLowerCase()}</p>
                     <p>Marque: ${car.brand}</p>
-                    <p>Garage: ${garage.title}</p>
+                    <p>Garage: ${garage ? garage.title : "inconnu"}</p>
                 </div>
             `;
-        break;
-      case "clientc":
-        carInfo += `
+            break;
+          case "clientc":
+            carInfo += `
                 <div style="background-color: ${car.colorHex}; padding: 10px; margin: 10px 0; color: white">
                     <p>Nom: ${car.modelName}</p>
                     <p>Marque: ${car.brand}</p>
                 </div>
             `;
-        break;
-      default:
-        carInfo += "<div>Client non reconnu.</div>";
-    }
+            break;
+          default:
+            carInfo += "<div>Client non reconnu.</div>";
+        }
+        carInfo += "</div>";
+      });
 
-    carsContent.append(carInfo + "</div>");
+    carsContent.append(carInfo);
   });
 }
